318401ae7f6ed78085ba06026e7dca057b502a1d
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-02-05 16:41:08 -0500

    various changes.

--- personal/prelude-ajerneck.el
+++ personal/prelude-ajerneck.el
@@ -54,7 +54,6 @@
 ;; Magit keybindings
 (global-set-key "\M-g\M-m" 'magit-status)
 
-
 ;; Org Mode customizations
 (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "CURRENT(u!)" "WAIT(w@)" "SOMEDAY(y)" "|" "DONE(d!)" "CANCELLED(c@)")))
 
@@ -63,6 +62,77 @@
 (setq org-default-notes-file (concat org-directory "/inbox.org"))
 (define-key global-map "\C-cc" 'org-capture)
 
+;; agenda view to get an overview of all projects.
+(setq org-agenda-custom-commands
+      '(("p" tags-todo "TODO=\"NEXT\"project:current|TODO=\"STARTED\"project:current|TODO=\"WAIT\"project:current")))
+
+;; remember templates
+(setq org-capture-templates
+      '(("t" "Task" entry (file+headline "~/start/admin/org/inbox.org" "Tasks")
+         "* TODO %? %^G \n  %U\n%a")
+        ("j" "Journal" entry (file+datetree "~/start/admin/org/journal.org")
+         "* %U Journal Entry\n%?\n
+
+** Goals for tomorrow
+")
+
+        ("r" "Resistance Planning" entry (file+datetree "~/start/admin/org/journal.org")
+         "* %U Plan\n
+        - [ ] 09:00
+        - [ ]
+        - [ ] 10:00
+        - [ ]
+        - [ ] 11:00
+        - [ ]
+        - [ ] 12:00
+        - [ ]
+        - [ ] 13:00
+        - [ ]
+        - [ ] 14:00
+        - [ ]
+        - [ ] 15:00
+        - [ ]
+        - [ ] 16:00
+        - [ ]
+        - [ ] 17:00
+        - [ ]
+        - [ ] 18:00
+        - [ ]
+        - [ ] 19:00
+        - [ ]
+
+*** Day Notes
+
+")
+
+        ("m" "Most important tasks" entry (file+datetree "~/start/admin/org/journal.org")
+         "* %U Most important tasks:\n
+** TODO %?\n
+** TODO  \n
+* Day notes
+")
+
+))
+
+;; orgmode remember setup
+; Targets include this file and any file contributing to the agenda -
+                                        ; up to 9 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 4)
+                                 (org-agenda-files :maxlevel . 4))))
+
+;; turn on auto-fill-mode globally
+;; (setq-default auto-fill-function 'do-auto-fill)
+
+;; turn on reftex mode
+(require 'reftex)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
+(add-hook 'org-mode-hook 'turn-on-reftex)
+;; specify the default bibliography, so reftex can find it.
+;; http://orgmode.org/worg/org-faq.html#using-reftex-in-org-mode
+(setq reftex-default-bibliography
+      (quote
+       ("/home/alexander/start/academic/lit/bibtex/master.bib")))
+(define-key org-mode-map (kbd "C-c )") 'reftex-citation)
 
 ;; One sentence per line.
 ;; To adjust one sentence per line
@@ -78,6 +148,60 @@
       (forward-sentence)
       (fill-region-as-paragraph beg (point)))))
 (global-set-key "\ej" 'fill-sentence)
+
+
+;; Highlight current line
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#585858")
+
+;; Move buffers around easily using buf-move
+;; http://www.johndcook.com/blog/2012/03/07/shuffling-emacs-windows/
+
+(require 'buffer-move)
+(global-set-key (kbd "C-x <up>")     'buf-move-up)
+(global-set-key (kbd "C-x <down>")   'buf-move-down)
+(global-set-key (kbd "C-x <left>")   'buf-move-left)
+(global-set-key (kbd "C-x <right>")  'buf-move-right)
+
+(add-hook 'org-mode-hook '(lambda ()
+                            (local-set-key (kbd "C-x <up>")     'buf-move-up)
+                            (local-set-key (kbd "C-x <down>")   'buf-move-down)
+                            (local-set-key (kbd "C-x <left>")   'buf-move-left)
+                            (local-set-key (kbd "C-x <right>")  'buf-move-right)
+                            )
+          )
+
+
+;; Show current buffer filename
+(defun show-file-name ()
+  "Show the full path file name in the minibuffer."
+  (interactive)
+  (message (buffer-file-name))
+  (kill-new (file-truename buffer-file-name))
+  )
+(global-set-key "\C-cz" 'show-file-name)
+
+;; Turn off smart underscore in ESS
+(ess-toggle-underscore nil)
+
+;; Activate polymodes
+;; https://github.com/vspinu/polymode
+(add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
+
+;; use cabal-repl with haskell mode.
+(custom-set-variables
+ '(haskell-process-type 'cabal-repl))
+
+
+;; redefine the prefix key for flycheck, because the default clobbers
+;; the org-time-stamp-inactive command.
+(define-key flycheck-mode-map flycheck-keymap-prefix nil)
+(setq flycheck-keymap-prefix (kbd "C-c f"))
+(define-key flycheck-mode-map flycheck-keymap-prefix
+  flycheck-command-map)
+
 
 
 (provide 'prelude-ajerneck)
